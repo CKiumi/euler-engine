e5e77083af183d60333e512a05ade8009b6fed4b
CKiumi <c.kiumi.oval17@gmail.com>
2022-06-08 16:51:52 +0900

    add support for num in parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -13,7 +13,7 @@ impl<'a> Parser<'a> {
         Parser { lexer }
     }
     pub fn parse(&mut self, end_token: &Token) -> Expr<'a> {
-        let mut expr = Expr::Num(Num::new(1));
+        let mut expr = Expr::Num(Num::new(i32::MAX));
         let mut last_token = Token::Eof;
         let mut is_last = false;
         while !is_last {
@@ -39,27 +39,35 @@ impl<'a> Parser<'a> {
                 }
                 Token::Sym(slice) => {
                     if let Token::AddInfix = last_token {
-                        expr = match expr {
-                            Expr::Sym(prev) => Expr::Add(prev + Sym::new(slice)),
-                            Expr::Add(prev) => Expr::Add(prev + Sym::new(slice)),
-                            Expr::Mul(prev) => Expr::Add(prev + Sym::new(slice)),
-                            _ => unimplemented!(),
-                        }
+                        expr = expr + Expr::Sym(Sym::new(slice));
                     } else {
                         expr = match expr {
-                            Expr::Sym(prev) => Expr::Mul(prev * Sym::new(slice)),
-                            Expr::Mul(mul) => Expr::Mul(mul * Sym::new(slice)),
                             Expr::Add(mut add) => {
                                 let last = add.exprs.pop().unwrap();
                                 Expr::Add(
                                     Add::new(add.exprs) + Mul::new(vec![last]) * Sym::new(slice),
                                 )
                             }
-                            _ => Expr::Sym(Sym::new(slice)),
+                            expr => match expr {
+                                Expr::Num(n) if n.num == i32::MAX => Expr::Sym(Sym::new(slice)),
+                                _ => expr * Expr::Sym(Sym::new(slice)),
+                            },
                         }
                     }
                     last_token = next_token;
                 }
+                Token::Num(num) => {
+                    if let Token::AddInfix = last_token {
+                        expr = expr + Expr::Num(num);
+                    } else {
+                        expr = match expr {
+                            Expr::Num(n) if n.num == i32::MAX => Expr::Num(num),
+                            _ => panic!("Number comes first or after +"),
+                        }
+                    }
+                    last_token = next_token;
+                }
+
                 _ => unimplemented!(),
             };
         }
@@ -80,11 +88,18 @@ pub fn latex_to_expr(latex: &str) -> Expr {
 }
 #[test]
 fn test_parser() {
-    println!("{}", latex_to_expr("aaaa"));
-    println!("{}", latex_to_expr("a+a+a"));
-    println!("{}", latex_to_expr("a+bc"));
-    println!("{}", latex_to_expr("ab+b\\alpha sdas+x"));
-    println!("{}", latex_to_expr("b^{a}"));
-    println!("{}", latex_to_expr("b_{a}"));
-    println!("{}", latex_to_expr("b_{a}^{c}"));
+    assert_eq!(latex_to_expr("aaaa").to_string(), "a*a*a*a");
+    assert_eq!(latex_to_expr("a+a+a").to_string(), "a+a+a");
+    assert_eq!(latex_to_expr("a+bc").to_string(), "a+b*c");
+    assert_eq!(
+        latex_to_expr("ab+b\\alpha sdas+x").to_string(),
+        "a*b+b*\\alpha*s*d*a*s+x"
+    );
+    assert_eq!(latex_to_expr("").to_string(), "2147483647");
+    assert_eq!(latex_to_expr("b^{a}").to_string(), "b^{a}");
+    assert_eq!(latex_to_expr("b_{a}").to_string(), "b_{a}");
+    assert_eq!(latex_to_expr("b_{a}^{c}").to_string(), "b_{a}^{c}");
+    assert_eq!(latex_to_expr("2a").to_string(), "2*a");
+    assert_eq!(latex_to_expr("23a").to_string(), "23*a");
+    assert_eq!(latex_to_expr("23a+23a").to_string(), "23*a+23*a");
 }
